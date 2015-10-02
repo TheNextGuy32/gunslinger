501fc49c0ef8ce26a0682aff3d758a380ac41f20
unknown <oeb9106@OH1380-12.main.ad.rit.edu>
2015-10-02 11:02:40 -0400

    Add basic canvas example

---
+++ canvas.js
@@ -0,0 +1,113 @@
+use strict;
+var canvas = document.getElementById(canvas);
+var ctx = canvas.getContext(2d);
+
+//  Keyboard input
+var keys = {};
+var oldKeys = {};
+addEventListener(keydown, function (e) 
+{
+	if(!keys.hasOwnProperty(e.keyCode))
+	{
+		  The key is newly down!
+    	keys[e.keyCode] = true;
+	}
+}, false);
+
+addEventListener(keyup, function (e) 
+{
+	if(keys.hasOwnProperty(e.keyCode))
+	{
+		  The key is newly released!
+    	delete keys[e.keyCode];
+	}
+
+}, false);
+
+function input(){
+	if ( keys[87] && !oldKeys[87]) {    W
+		console.log('W');
+	}
+	else if (keys[68] ) {    D
+		console.log('D');
+	}
+
+	else if ( keys[65] ) {    A
+		console.log('A');
+	}
+	if ( keys [83] ) {    S
+		console.log('S');
+	}
+
+	if((keys[68]  keys[65]))
+	{
+		if ( keys[32] && !oldKeys[32]) {    SPACE
+
+		}
+
+		
+	}
+}
+
+var things = new Array();
+
+var camX = 0;
+var camY = 0;
+
+function init() {
+
+	//  Initialization code
+
+	animFrame( recursiveAnim );
+}
+
+function update() {
+	
+	//  Update code
+	for(var t  = 0 ; t  things.length; t++)
+	{
+		things[t].accelerationX = 0;
+		things[t].accelerationY = 0;
+
+		//  Handle velocity
+
+		//  Handle position
+	}
+
+	input();
+
+	//  Maintain camera
+	//camX = cena.worldX;
+	//camY = cena.worldY;
+	oldKeys = $.extend( {}, keys );
+}
+function draw() {
+	
+	//  Draw code
+
+	ctx.save();
+	ctx.fillStyle = 'gray';
+	ctx.fillRect(0,0,800,600);
+	ctx.restore();
+	
+}
+
+
+
+
+
+window.onload = init;
+var animFrame = 
+	window.requestAnimationFrame   			
+    window.webkitRequestAnimationFrame    	
+    window.mozRequestAnimationFrame    		
+    window.oRequestAnimationFrame      		
+    window.msRequestAnimationFrame     		
+    null ;
+
+
+var recursiveAnim = function() {
+    update();
+    draw();
+    animFrame( recursiveAnim );
+};
