656033e5dead9c2e5f54a0c66b7f28be4a95e78c
rxm2995 <rxm2995@rit.edu>
2015-10-09 14:25:22 -0400

    Create Vector class
    And implement it in Movable.

---
+++ js/vector.js
@@ -0,0 +1,65 @@
+function Vector(initX, initY)
+{
+	this.x = initX;
+	this.y = initY;
+	
+	this.getMagSquared= function()
+	{
+		return this.x*this.x+this.y*this.y;
+	};
+	
+	this.getMag= function()
+	{
+		return Math.sqrt(this.getMagSquared());
+	};
+	
+	this.setMag= function(newMag)
+	{
+		var mag = this.getMag();
+		this.x *= newMag/mag;
+		this.y *= newMag/mag;
+	};
+	
+	this.normalize= function()
+	{
+		var mag = this.getMag();
+		this.x /= mag;
+		this.y /= mag;
+	};
+	
+	this.add= function(otherVec)
+	{
+		return new Vector(this.x+otherVec.x, this.y+otherVec.y);
+	}
+	
+	this.sub= function(otherVec)
+	{
+		return new Vector(this.x-otherVec.x, this.y-otherVec.y);
+	}
+	
+	this.mult= function(num)
+	{
+		return new Vector(this.x*num, this.y*num);
+	}
+	
+	this.div= function(num)
+	{
+		return new Vector(this.x/num, this.y/num);
+	}
+	
+	this.dot= function(otherVec)
+	{
+		return this.x*otherVec.x+this.y*otherVec.y;
+	};
+	
+	this.copy = function()
+	{
+		return new Vector(this.x, this.y);
+	};
+	
+	this.toString= function()
+	{
+		return "("+this.x+", "+this.y+")";
+	};
+	
+}
